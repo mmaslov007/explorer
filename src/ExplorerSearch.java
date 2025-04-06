cb6ea96f5075d55d65311fd33d72f16ac8017ff7
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-04-06 13:56:02 -0700

    Exploring methods created and verified with existing test, will make adjustments if neccessary while adding new tests.

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -29,9 +29,52 @@ public class ExplorerSearch {
      * @return the number of spaces the explorer can reach
      */
     public static int reachableArea(int[][] island) {
-        // Implement your method here!
-        // Please also make more test cases
-        // I STRONGLY RECOMMEND testing some helpers you might make too
-        return -1;
+        int[] start = explorerLocation(island);
+        boolean[][] visited = new boolean[island.length][island[0].length];
+        return areaSearch(island, visited, start[0], start[1]);
+    }
+
+    // recursive searching algorithm
+    private static int areaSearch(int[][] island, boolean[][] visited, int row, int col) {
+        if (!isValid(island, visited, row, col)) return 0;
+
+        visited[row][col] = true;
+        int count = 1;
+
+        for (int[] move : possibleMoves(row, col)) {
+            count += areaSearch(island, visited, move[0], move[1]);
+        }
+
+        return count;
+    }
+
+    // checks if the next move is valid (not 2 or 3)
+    private static boolean isValid(int[][] island, boolean[][] visited, int row, int col) {
+        if (row < 0 || col < 0 || row >= island.length || col >= island[0].length) return false;
+        if (visited[row][col]) return false;
+        int val = island[row][col];
+        return val == 0 || val == 1;
+    }
+
+    // tracks all possible moves from a given position
+    private static List<int[]> possibleMoves(int row, int col) {
+        List<int[]> moves = new ArrayList<>();
+        moves.add(new int[]{row - 1, col}); // up
+        moves.add(new int[]{row + 1, col}); // down
+        moves.add(new int[]{row, col - 1}); // left
+        moves.add(new int[]{row, col + 1}); // right
+        return moves;
+    }
+
+    // finds the explorer location and throws exception if none found
+    public static int[] explorerLocation(int[][] island) {
+        for (int r = 0; r < island.length; r++) {
+            for (int c = 0; c < island[r].length; c++) {
+                if (island[r][c] == 0) {
+                    return new int[]{r, c};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No explorer found.");
     }
 }
